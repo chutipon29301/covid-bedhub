e8e4b5fa8788dafaae550e9f0204ec7fc160992c
wchr-aun <wchr.aun@gmail.com>
2021-07-21 01:38:13 +0700

    refactor(guards): to update the auth guard

--- src/lib/guards/index.ts
+++ src/lib/guards/index.ts
@@ -1,20 +1,53 @@
-import { PROTECTED_ROUTES, ROUTES } from '$lib/constants/routes';
+import {
+	HEALTHCARE_CODE_ROUTES,
+	HEALTHCARE_QUEUE_ROUTES,
+	REPORTER_ROUTES,
+	HEALTHCARE_STAFF_ROUTES,
+	PROTECTED_ROUTES,
+	ROUTES
+} from '$lib/constants/routes';
 import { isLogin$ } from '$lib/store';
 import type { LoadInput, LoadOutput } from '@sveltejs/kit';
+import { AccountType } from '$lib/models';
+import type { DecodedJwt } from '$lib/models';
+import jwt_decoder from 'jwt-decode';
+import cookie from 'cookie';
 
 let isLogin = false;
 const protectedPath = PROTECTED_ROUTES;
 isLogin$.subscribe((authState) => (isLogin = authState));
 
 export async function authGuard({ session, page }: LoadInput): Promise<LoadOutput> {
+	// if (import.meta.env.VITE_DEVELOP) return {};
+
 	const { access_token } = session;
-	if (!access_token && !isLogin && protectedPath.includes(page.path)) {
+	const loggedIn = access_token || isLogin;
+
+	if (!loggedIn && protectedPath.includes(page.path)) {
 		return { status: 302, redirect: ROUTES.LANDING };
-	} else if ((access_token || isLogin) && page.path === ROUTES.LOGIN) {
-		return { status: 302, redirect: ROUTES.HOME };
-	} else {
-		return {};
 	}
+
+	if (!loggedIn) return {};
+
+	const { accountType } = jwt_decoder<DecodedJwt>(
+		access_token || cookie.parse(document.cookie).access_token
+	);
+
+	if (accountType === AccountType.CODE && routeCheck(page.path, HEALTHCARE_CODE_ROUTES))
+		return { status: 302, redirect: ROUTES.HEALTHCARE_CODE };
+	else if (accountType === AccountType.QUEUE && routeCheck(page.path, HEALTHCARE_QUEUE_ROUTES))
+		return { status: 302, redirect: ROUTES.HEALTHCARE_QUEUE };
+	else if (accountType === AccountType.STAFF && routeCheck(page.path, HEALTHCARE_STAFF_ROUTES))
+		return { status: 302, redirect: ROUTES.HEALTHCARE_STAFF };
+	else if (accountType === AccountType.REPORTER && routeCheck(page.path, REPORTER_ROUTES))
+		return { status: 302, redirect: ROUTES.HOME };
+
+	return {};
+}
+
+function routeCheck(path: string, routes: string[]) {
+	if (path === '/') return false;
+	return !routes.some((r) => path.includes(r));
 }
 
 export default {
